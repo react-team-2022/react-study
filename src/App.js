62993221d3a0285bc4a5a5016bb0894c85bdbd74
umcondo <superpowerkdg@gmail.com>
2022-05-29 19:54:37 +0900

    router를 이용한 page분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-/* components*/
-import Content from "./components/Content";
+/* Pages */
+import Chu from "./Pages/Chu";
+import Daeguk from "./Pages/Daeguk";
+import G1 from "./Pages/G1";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
 
 function App() {
+  const members = [
+    {
+      id: 0,
+      memberName: "추홍규",
+      path: "chu",
+    },
+    {
+      id: 1,
+      memberName: "강대국",
+      path: "daeguk",
+    },
+    {
+      id: 2,
+      memberName: "민지원",
+      path: "g1",
+    },
+  ];
+
   return (
-    <div className="App">
-      <Content />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home members={members} />} />
+          <Route path="/g1" element={<G1 members={members} />} />
+          <Route path="/daeguk" element={<Daeguk members={members} />} />
+          <Route path="/chu" element={<Chu members={members} />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,13 @@
-const Button = function ({ member, idx, changeMember }) {
-  return <button onClick={changeMember}>{member}</button>;
+import { useNavigate } from "react-router-dom";
+
+const Button = function ({ memberName, path }) {
+  const navigate = useNavigate();
+  const pageRoutes = (e) => {
+    if (e.target.textContent === memberName) {
+      navigate("/" + path);
+    }
+  };
+  return <button onClick={pageRoutes}>{memberName}</button>;
 };
 
 export default Button;
