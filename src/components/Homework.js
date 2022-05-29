e147af46131344af503cdcf3bb65e244a24204dc
umcondo <superpowerkdg@gmail.com>
2022-05-30 00:19:21 +0900

    Add styled-component

--- src/components/Homework.js
+++ src/components/Homework.js
@@ -1,19 +1,36 @@
 import { useState } from "react";
+import styled from "styled-components";
 
+const Hover = styled.div`
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #ffffff;
+  }
+`;
 const Homework = ({ id, title, description }) => {
   const [active, setActive] = useState(true);
   const isShow = () => {
     setActive(!active);
   };
   return (
-    <div className="homework">
+    <div>
       {/* Homework 제목 */}
-      <h3
-        onClick={isShow}
-        style={{ display: "inline-block", background: "rgba(0, 0, 0, 0.2)" }}
-      >
-        {id}) {title}
-      </h3>
+      <Hover>
+        <h3
+          onClick={isShow}
+          style={{
+            display: "inline-block",
+            background: "rgba(0, 0, 0, 0.2)",
+            margin: "10px 20px",
+          }}
+        >
+          {id}) {title}
+        </h3>
+      </Hover>
       {/* Homework 내용 조건부 렌더링 */}
       {active && (
         <ul>
