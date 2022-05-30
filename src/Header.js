eafb68882557c8146a7019938d54d41b3fa3b627
MinJiwon <jwjwmin111@gmail.com>
2022-05-30 13:11:37 +0900

    styled-components 사용

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderStyle = styled.section`
+  background-color: lightblue;
+  p {
+    color: gray;
+  }
+`;
+
+const H1Style = styled.h1`
+  background-color: lightpink;
+`;
 
 const Header = ({ title, description, active, id }) => {
   return (
-    <>
-      <h1>{title}</h1>
+    <HeaderStyle>
+      <H1Style>{title}</H1Style>
       <p>{description}</p>
       <p>active: {active ? "true입니다" : "false입니다"}</p>
       <p>id: {id}</p>
-    </>
+    </HeaderStyle>
   );
 };
 
