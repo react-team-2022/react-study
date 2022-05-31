a431e682bfd2a2aecff10df39f46a8eaa6ebd231
MinJiwon <jwjwmin111@gmail.com>
2022-05-31 17:22:25 +0900

    input연습

--- src/components/G1.js
+++ src/components/G1.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Header from "./Header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 1em;
+  > * {
+    margin-bottom: 0.5em;
+  }
+  input,
+  button {
+    padding: 0.5em;
+  }
+`;
 
 const G1 = () => {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText("");
+  };
+
   return (
     <>
       <Header />
-      <h1>😎</h1>
+      <Wrapper>
+        <h1>😎</h1>
+        <input onChange={onChange} value={text} />
+        <button onClick={onReset}>초기화</button>
+        <p>결과: {text}</p>
+        <hr />
+      </Wrapper>
     </>
   );
 };
