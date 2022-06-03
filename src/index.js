fddf5e3113d9fb8a2c65170681af5b3d9a14246b
umcondo <superpowerkdg@gmail.com>
2022-06-03 16:03:22 +0900

    Add globalStyle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  body{background:lightblue}
+`;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
