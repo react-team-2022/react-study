50c2a46e5f7d080555465cd63f5598d326dc2541
Mr.chu <chuhongkyu@gmail.com>
2022-06-09 17:33:29 +0900

    댓글창

--- src/components/Exam8.js
+++ src/components/Exam8.js
@@ -3,36 +3,45 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 60vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 100vh;
   background-color: white;
   border: 2px solid black;
   color: black;
   padding: 10px;
-  div {
-    width: 50%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
-  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
   ul {
-    width: 50%;
+    width: 70%;
     height: 100%;
     li {
-      height: 50px;
+      height: 100px;
       text-align: center;
       color: rgba(0, 0, 0, 0.7);
-      padding: 10px;
+      padding: 10px 50px;
       display: flex;
-      justify-content: space-around;
+      justify-content: space-between;
       align-items: center;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      margin-bottom: 10px;
       span {
-        background-color: white;
+        margin-right: 5px;
+        padding: 9px 10px;
+        background-color: rgb(241, 241, 241);
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+          rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+        cursor: pointer;
+        border-radius: 50%;
         &:hover {
-          background-color: black;
+          transform: scale(1.2);
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+            rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
         }
       }
     }
@@ -40,27 +49,70 @@ const Wrapper = styled.div`
 `;
 
 const InputBox = styled.section`
-  width: 50%;
+  width: 70%;
+  height: 200px;
   display: flex;
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
+  background-color: rgb(247, 247, 247);
+  padding: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  position: relative;
+  input {
+    border: none;
+    border-bottom: 2px solid black;
+    padding: 10px;
+    margin-bottom: 5px;
+  }
+  textarea {
+    width: 100%;
+    height: 6.25em;
+    border: none;
+    resize: none;
+  }
+  button {
+    position: absolute;
+    z-index: 5;
+    right: 50px;
+    bottom: 50px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+    &:hover {
+      background-color: gray;
+      color: white;
+    }
+  }
+`;
+
+const ProfilCircle = styled.div`
+  width: 60px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(58, 170, 91);
+  color: white;
+  border-radius: 50%;
 `;
 
 const Exam8 = () => {
-  const [inputs, setInputs] = useState({ username: "", type: "front-end" });
-  const { username, type } = inputs;
+  const [inputs, setInputs] = useState({
+    username: "",
+    comment: "",
+    active: false,
+  });
+  const { username, comment } = inputs;
   const onChange = (e) => {
     const { name, value } = e.target;
     setInputs({ ...inputs, [name]: value });
   };
   const [users, setUsers] = useState([
-    { id: 1, username: "추홍규", type: "front-end" },
-    { id: 2, username: "김연준", type: "back-end" },
-    { id: 3, username: "이경은", type: "front-end" },
-    { id: 4, username: "김대희", type: "back-end" },
-    { id: 5, username: "민지원", type: "front-end" },
-    { id: 6, username: "강대국", type: "front-end" },
+    { id: 1, username: "추홍규", comment: "하기 싫다", active: false },
+    { id: 5, username: "민지원", comment: "열심히 하셔야죠.", active: false },
+    { id: 6, username: "강대국", comment: "열심히 하셔야죠", active: false },
   ]);
 
   const nextId = useRef(7);
@@ -69,10 +121,10 @@ const Exam8 = () => {
     const user = {
       id: nextId.current,
       username,
-      type,
+      comment,
     };
     setUsers([...users, user]);
-    setInputs({ username: "", type: "front-end" });
+    setInputs({ username: "", comment: "" });
     nextId.current += 1;
     console.log(user);
   };
@@ -81,53 +133,56 @@ const Exam8 = () => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
-  const onFind = (type) => {
-    setUsers(users.filter((user) => user.type === type));
-  };
-
-  const onExit = (id) => {
+  const onToggle = (id) => {
     setUsers(
-      users.map((user) => (user.id === id ? { ...user, type: "취업" } : user))
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
     );
   };
   return (
     <Wrapper>
-      <div>
+      <h1>댓글창 ({users.length})</h1>
+      <Container>
         <ul>
           {users.map((file, index) => (
             <li
               style={
-                file.type === "front-end"
-                  ? { backgroundColor: "hotpink" }
-                  : { backgroundColor: "skyblue" }
+                !file.active
+                  ? { backgroundColor: "white" }
+                  : { backgroundColor: "hotpink" }
               }
               key={index}
             >
-              {file.username}
-              <span onClick={() => onRemove(file.id)}>✖️</span>
-              <span onClick={() => onExit(file.id)}>✔️</span>
+              <ProfilCircle>{file.username}</ProfilCircle>
+              <div>
+                <h2>{file.username}</h2>
+                <p>{file.comment}</p>
+              </div>
+              <div>
+                <span onClick={() => onRemove(file.id)}>✖️</span>
+                <span onClick={() => onToggle(file.id)}>♥</span>
+              </div>
             </li>
           ))}
         </ul>
-      </div>
-      <InputBox>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          placeholder="성함"
-          onChange={onChange}
-        />
-        <select name="type" value={type} placeholder="소속" onChange={onChange}>
-          <option value="front-end" selected>
-            프론트 엔드
-          </option>
-          <option value="back-end">백 엔드</option>
-        </select>
-        <button onClick={onCreate}>등록</button>
-        <button onClick={() => onFind("front-end")}>프론트 엔드</button>
-        <button onClick={() => onFind("back-end")}>백 엔드</button>
-      </InputBox>
+        <InputBox>
+          <input
+            comment="text"
+            name="username"
+            value={username}
+            placeholder="성함"
+            onChange={onChange}
+          />
+          <textarea
+            name="comment"
+            value={comment}
+            placeholder="내용없음"
+            onChange={onChange}
+          ></textarea>
+          <button onClick={onCreate}>등록</button>
+        </InputBox>
+      </Container>
     </Wrapper>
   );
 };
